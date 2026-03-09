c7eafa20f7dc411bd903610e03be5d177987f3d7
Chapparapu Sri Nandhika <nandhikachapparapu@gmail.com>
2026-03-09 19:16:51 +0530

    Add lowercase to uppercase conversion logic
    Implement a method to convert lowercase letters to uppercase without using built-in functions.

---
+++ Convert lowercase → uppercase without built-in functions.java
@@ -0,0 +1,18 @@
+import java.util.*;
+//Convert lowercase → uppercase without built-in functions
+public class Main {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+String s = sc.nextLine();
+char[] ch=s.toCharArray();
+for(int i=0;i<s.length();i++){
+    if(ch[i]>='a' && ch[i]<='z'){
+        ch[i]-=32;
+    }
+}
+for(char c:ch){
+    System.out.print(c+" ");
+}
+sc.close();
+}
+}
